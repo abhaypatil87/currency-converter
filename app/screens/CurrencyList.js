9d5dce916547ae42b08f8b053d539512fb7cb65a
Patil, Mangauda <abhay.patil@pearson.com>
2019-07-11 22:16:54 +0200

    Add Search Filter to the Currency List.

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FlatList, View, StatusBar } from 'react-native';
-import { ListItem, Divider } from 'react-native-elements';
+import { ListItem, Divider, SearchBar } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -8,12 +8,34 @@ import currencies from '../data/currencies';
 import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
 class CurrencList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currenciesList: currencies,
+    };
+  }
   static propTypes = {
     navigation: PropTypes.object,
     dispatch: PropTypes.func,
     baseCurrency: PropTypes.string,
     quoteCurrency: PropTypes.string,
     primaryColor: PropTypes.string,
+  };
+
+  filter = (text) => {
+    this.setState({
+      value: text,
+    });
+  
+    let filteredCurrencies = currencies.filter(item => {
+      const itemData = `${item.code.toUpperCase()} ${item.text.toUpperCase()}`;
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+
+    this.setState({
+      currenciesList: filteredCurrencies,
+    })
   };
 
   handlePress = (currency) => {
@@ -26,6 +48,19 @@ class CurrencList extends Component {
     this.props.navigation.goBack(null);
   };
 
+  renderHeader = () => {
+    return (
+      <SearchBar 
+        placeholder="Search Currencies"
+        lightTheme
+        round
+        onChangeText={text => this.filter(text)}
+        autoCorrect={false}
+        value={this.state.value}
+      />
+      );
+  };
+
   render() {
     let comparisonCurrency = this.props.baseCurrency;
     if (this.props.navigation.state.params.type === 'quote') {
@@ -35,7 +70,7 @@ class CurrencList extends Component {
       <View style={{ flex: 1 }}>
         <StatusBar barStyle="default" translucent={false} />
         <FlatList
-          data={currencies}
+          data={this.state.currenciesList}
           renderItem={({ item }) => (
             <ListItem
               title={item.code + ' - ' + item.text}
@@ -45,6 +80,7 @@ class CurrencList extends Component {
           )}
           keyExtractor={item => item.code}
           ItemSeparatorComponent={Divider}
+          ListHeaderComponent={this.renderHeader}
         />
       </View>
     );
